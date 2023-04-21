2d4cbe9d7c2e067390513a51e0201a8cadf2b02d
fatisdd <saddiki.fati01@gmail.com>
2023-04-20 19:46:00 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main -A c progrqm that prints a line to the standars error
+ * Return 1 (Success)
+ */
+int main(void)
+{
+	fput("and that oiece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
